d30b695b0fcb476e7463d184414fe66691348a4d
innersouce hide <developer@softnepo.com>
2024-10-13 18:34:40 -0300

    testing wifi

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ac01
+
+This project is experiment freeRTS within esp32 dev kit
+
+## Useful command
+
+
+## References
+[Keep WiFi connection alive with a FreeRTOS task](https://simplyexplained.com/blog/esp32-keep-wifi-alive-with-freertos-task/)
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,67 @@
 #include <Arduino.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <WiFi.h>
+#include <iostream>
 
-// put function declarations here:
-int myFunction(int, int);
+#define WIFI_NETWORK "--- your WiFi network name ---"
+#define WIFI_PASSWORD "--- your WiFi password ---"
+
+const long WIFI_TIMEOUT_MS = 10000; // 20 second WiFi connection timeout
+const long WIFI_RECOVER_TIME_MS = 10000; // Wait 30 seconds after a failed connection attempt
+
+// instances tasks
+TaskHandle_t th_wf = NULL;
+
+void xTaskKeepWiFiAlive(void *p);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    
+    pinMode(13, OUTPUT);
+
+    xTaskCreate(
+      xTaskKeepWiFiAlive,
+      "wifi_Debug",
+      (configMINIMAL_STACK_SIZE * 5),
+      NULL,
+      1,
+      &th_wf
+    );
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Implementations
+void xTaskKeepWiFiAlive(void *p) {
+   for(;;) {
+     
+     if (WiFi.status() == WL_CONNECTED)
+     {
+        digitalWrite(13, LOW);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        continue;
+     }
+     
+     Serial.println("[WIFI] Connecting");
+     WiFi.mode(WIFI_STA);
+     WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+     
+     unsigned long startAttemptTime = millis();
+
+     while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS){}
+     
+     if(WiFi.status() != WL_CONNECTED){
+        Serial.println("[WIFI] FAILED");
+
+        digitalWrite(13, HIGH);
+        vTaskDelay(WIFI_RECOVER_TIME_MS / portTICK_PERIOD_MS);
+			  continue;
+      }
+
+      Serial.println("[WIFI] Connected: " + WiFi.localIP());
+      digitalWrite(13, LOW);
+   }
 }
