e2073af276c8e8cddf702f0f6feba165314fb1b6
Lucas <lucas.nepomuceno1999@gmail.com>
2024-10-29 11:51:25 -0300

    fast test with wifi + post http

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,16 @@
 #include <freertos/task.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <iostream>
+#include <string>
+#include <codecvt>
 
-const int LED_WIFI_OFF = 2;
+const int SEN_LDR = 33;
+const int LED_WIFI_ON = 2;
+const int LED_POLES = 15;
+
+const int LDR_READ = 34;
+
 const long WIFI_TIMEOUT_MS = 2000;
 const long WIFI_RECOVER_TIME_MS = 2000;
 
@@ -17,33 +25,58 @@ void xTaskRule(void *p);
 
 void setup() {
     Serial.begin(115200);
+    
+    analogReadResolution(SEN_LDR);
 
-    pinMode(2, OUTPUT);
-    pinMode(15, OUTPUT);
-  
+    pinMode(LED_WIFI_ON, OUTPUT);
+    pinMode(LED_POLES, OUTPUT);
+    pinMode(LDR_READ, INPUT);
+
     xTaskCreate(xTaskKeepWiFiAlive,"wifi_Debug",(configMINIMAL_STACK_SIZE * 3), NULL, 2, &th_wf);
     xTaskCreate(xTaskRule,         "rule_Debug",(configMINIMAL_STACK_SIZE * 5) ,NULL, 1, &rl_wf);
 }
 
 void loop() { }
 
+String getLDR() {
+  int analogValue = analogRead(SEN_LDR);
+  int a = (analogValue / 4095.0) * 100;
+  String postData = "{\"SL\":"+ String(a)+"}";
+  return postData;
+}
+
+// Implementations
 void xTaskRule(void *p) {
   while(1)  {
-    digitalWrite(15, HIGH);
     vTaskDelay(3000 / portTICK_PERIOD_MS);  
-     
+
      if(WiFi.status() == WL_CONNECTED) {
+
         HTTPClient http;   
         http.begin("http://192.168.43.119:3000/");  
-        http.addHeader("Content-Type", "text/plain"); 
-        
-        int httpResponseCode = http.GET();   
-        
+        http.addHeader("Content-Type", "application/json"); 
+
+        String body = getLDR();
+        Serial.print("Preparando Response: ");
+        Serial.println(body);
+        int httpResponseCode = http.POST(body);   
+
         if(httpResponseCode > 199 && httpResponseCode < 300){
-          digitalWrite(15, LOW);
-          String response = http.getString();   
-          Serial.println(httpResponseCode); 
-          Serial.println(response);
+          String response = http.getString();
+          char power = (int) response[0];
+
+          
+          Serial.print("Power : ");
+          Serial.print(power);
+          Serial.println("");
+          Serial.print("response : ");
+          Serial.print(response);
+          
+
+          if((int) response[1] == 0) {}
+          if((int) response[2] == 0) {}
+
+          digitalWrite(LED_POLES, power);
         }else{
           Serial.print("Error on sending POST: ");
           Serial.println(httpResponseCode);
@@ -60,7 +93,7 @@ void xTaskKeepWiFiAlive(void *p) {
    while(1) {
      
      if (WiFi.status() == WL_CONNECTED) {
-        digitalWrite(LED_WIFI_OFF, HIGH);
+        digitalWrite(LED_WIFI_ON, HIGH);
         continue;
      }
      
@@ -71,11 +104,12 @@ void xTaskKeepWiFiAlive(void *p) {
      unsigned long startAttemptTime = millis();
 
      while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS){
-       digitalWrite(LED_WIFI_OFF, LOW);
+       digitalWrite(LED_WIFI_ON, LOW);
      }
 
-     if(WiFi.status() != WL_CONNECTED) {        
-	continue;
+     if(WiFi.status() != WL_CONNECTED) {    
+        digitalWrite(LED_WIFI_ON, LOW);    
+			  continue;
       }
    }
 }
